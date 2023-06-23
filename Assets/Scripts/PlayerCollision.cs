0fbd8474f5a9d06ed305c271ff4c342382b65044
bittar <bittar@gmail.com>
2023-06-23 19:30:38 -0300

    Fixed Collision

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -39,6 +39,7 @@ public class PlayerCollision : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision)
     {
+        Vector3 impactForce = collision.relativeVelocity;
 
         if (collision.collider.tag == "Obstacle" && sideTouch == false)
         {
@@ -46,15 +47,30 @@ public class PlayerCollision : MonoBehaviour {
             followplayer.enabled = false;
             movement.rb.constraints = RigidbodyConstraints.None;
             FindObjectOfType<GameManager>().endGame();
+
+            // Adds Destroyed Version of the player and destroys/removes main player.
+            GameObject newObj = Instantiate(playerDestroyed, transform.position, transform.rotation);
             Destroy(gameObject);
-            Instantiate(playerDestroyed, transform.position, transform.rotation);
+
+            //Adds Velocity to Detroyed Player Children's Rigid Body
+            float variationScale = 0.5f;
+            float impactForceMag = impactForce.magnitude;
+
+            foreach(Transform child in newObj.transform)
+            {
+                Rigidbody rb = child.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddForce((impactForce + (Random.insideUnitSphere * variationScale * impactForceMag)) * -1, ForceMode.Impulse);
+                }
+            }
         }
 
         if (collision.collider.tag == "Ground")
         {
             //movement.inAirMove = 0;
-            movement.rb.angularVelocity = new Vector3(0f, 0f, 0f);
-            movement.transform.Rotate(0f, 0f, 0f);
+            //movement.rb.angularVelocity = new Vector3(0f, 0f, 0f);
+            //movement.transform.Rotate(0f, 0f, 0f);
 
         }
         
